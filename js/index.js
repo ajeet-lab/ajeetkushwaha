a333cfd8e8103a6e5ec53fb223771ab0c6c5e115
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2023-02-25 13:55:38 +0530

    asdasd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
  
- let typed = new Typed('#hero-left-head-span2', {
+let typed = new Typed('#hero-left-head-span2', {
     strings: ['Java Developer.', 'Full Stack Developer.','Web Designer.'],
     typeSpeed: 20,
     backSpeed: 20,
@@ -11,8 +11,9 @@
   });
 
 
-  let footerYear = document.getElementById("footer_Year");
+let footerYear = document.getElementById("footer_Year");
   console.log(footerYear)
   let date = new Date().getFullYear();
   console.log("date", date)
   footerYear.innerHTML = date;
+  
